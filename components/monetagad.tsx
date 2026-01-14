bcbbf1fbfcc134f4481a17c6604ce7038d76fbbf
abhaysachan232 <abhaysachan232@gmail.com>
2026-01-14 21:16:26 +0530

    op

--- components/monetagad.tsx
+++ components/monetagad.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import Script from "next/script";
+import { useEffect } from "react";
 
-export default function MonetagAd() {
-  return (
-    <Script
-      src="https://al5sm.com/tag.min.js"
-      strategy="afterInteractive"
-      data-zone="10402380"
-    />
-  );
+export default function MonetagQuge() {
+  useEffect(() => {
+    // prevent duplicate load
+    if (document.querySelector('script[src*="quge5.com/88/tag.min.js"]')) {
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.src = "https://quge5.com/88/tag.min.js";
+    script.async = true;
+    script.setAttribute("data-zone", "202023");
+    script.setAttribute("data-cfasync", "false");
+
+    document.body.appendChild(script);
+  }, []);
+
+  return null; // Monetag auto inject karta hai ads
 }
